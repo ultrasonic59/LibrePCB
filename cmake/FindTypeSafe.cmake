7efb474b26cc003c1ad00dea14f880b0eb8f1bb1
Danilo Bargen <mail@dbrgn.ch>
2020-10-26 00:56:22 +0100

    CMake: Add FindOptional
    Because the library directly exports the include directory, the
    namespacing for the includes needed to be removed.

--- cmake/FindTypeSafe.cmake
+++ cmake/FindTypeSafe.cmake
@@ -3,7 +3,11 @@ if(EXISTS "${TYPE_SAFE_SUBMODULE_BASEPATH}")
     message(STATUS "Using vendored TypeSafe")
 
     # Include local submodule
-    add_subdirectory("${TYPE_SAFE_SUBMODULE_BASEPATH}" build EXCLUDE_FROM_ALL)
+    add_subdirectory(
+        "${TYPE_SAFE_SUBMODULE_BASEPATH}"
+        "${CMAKE_BINARY_DIR}/libs/type_safe"
+        EXCLUDE_FROM_ALL
+    )
 
     # Alias static lib to namespaced variant
     add_library(TypeSafe::TypeSafe ALIAS type_safe)
