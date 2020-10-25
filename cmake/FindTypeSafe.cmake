c4d242b567f0ddd166e9ce91e4f5a8e3ba3216c6
Danilo Bargen <mail@dbrgn.ch>
2020-10-26 00:08:32 +0100

    Start setting up CMake build system

---
+++ cmake/FindTypeSafe.cmake
@@ -0,0 +1,15 @@
+set(TYPE_SAFE_SUBMODULE_BASEPATH "${PROJECT_SOURCE_DIR}/libs/type_safe")
+if(EXISTS "${TYPE_SAFE_SUBMODULE_BASEPATH}")
+    message(STATUS "Using vendored TypeSafe")
+
+    # Include local submodule
+    add_subdirectory("${TYPE_SAFE_SUBMODULE_BASEPATH}" build EXCLUDE_FROM_ALL)
+
+    # Alias static lib to namespaced variant
+    add_library(TypeSafe::TypeSafe ALIAS type_safe)
+
+    # Stop here, we're done
+    return()
+endif()
+
+message(FATAL_ERROR "Did not find TypeSafe library submodule, did you clone recursively?")
